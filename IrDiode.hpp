d00e3dea2e2f6fac6257027d7074cd918d7308b6
OscarKromhout <oscarkromhout@gmail.com>
2019-10-29 13:52:58 +0100

    irDiode klasse af en doxygen toegevoegd

---
+++ IrDiode.hpp
@@ -0,0 +1,28 @@
+#ifndef IRDIODE_HPP
+#define IRDIODE_HPP
+#include "../hwlib/library/hwlib.hpp"
+
+///\brief
+///IrDiode class for setting the IR sender pin high or low.
+class IrDiode{
+private:
+    hwlib::pin_out& diodePin;
+public:
+    ///\brief 
+    /// constructor
+    ///\details
+    ///this constructor only needs a reference to a hwlib::pin_out in his parameters to compile. The constructor also sets the pin low during start up
+    IrDiode(hwlib::target::pin_out& diodePin):
+    diodePin(diodePin){
+        diodePin.write(0);
+        diodePin.flush();
+    };
+
+    ///\brief
+    /// function to set the IR pin high.
+    void setHigh();
+    ///\brief
+    /// function to set the IR pin low.
+    void setLow();
+};
+#endif //IRDIODE_HPP
